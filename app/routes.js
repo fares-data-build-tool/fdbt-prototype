04f7b36123c07c3ece0bef950c6853f5c582d827
DarrenSilcox-MOJ <darren.silcox@digital.justice.gov.uk>
2020-07-31 08:57:25 +0100

    Alt to summary page for time restrictions and time restriction alterations and fixes to routing

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,56 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Profile 3 directions
+
+
+// Have you had a school experience before
+router.post('/v4r1/time-restrict', function (req, res) {
+
+    let decide = req.session.data['decide-yn']
+
+    console.log(req.session.data['decide-yn'])
+
+    //Option 1
+    if (decide === 'yes') {
+        res.redirect('/v4r1/time-restrict-day-week')
+    }
+    //Option 2
+    else if (decide === 'no') {
+        res.redirect('/v4r1/period-type')
+    }
+    //end
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router
