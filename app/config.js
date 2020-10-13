dd6631b738e92eb20dec7bd2e080ee57040b8a65
DarrenSilcox-MOJ <darren.silcox@digital.justice.gov.uk>
2020-10-13 11:53:40 +0100

    Change name of service

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Fares data build tool',
+  serviceName: 'Create Fares Data',
 
   // Default port that prototype runs on
   port: '3000',
