fcf97500ffa7c35a476f7ee2adb62932fba2f1aa
DarrenSilcox-MOJ <darren.silcox@digital.justice.gov.uk>
2020-11-04 11:28:34 +0000

    First school tickets

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Create Fares Data',
+  serviceName: 'Create fares data',
 
   // Default port that prototype runs on
   port: '3000',
